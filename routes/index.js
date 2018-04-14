dfdaaac9c81733cea67b41e51f75c68ba9f018af
Docomo4 <mattijs.leon@gmail.com>
2018-04-14 22:44:33 +0200

    order by half gefixt, kan alleen refinen op categorie ik ga hier nog even over nadenken

--- routes/index.js
+++ routes/index.js
@@ -95,7 +95,43 @@ router.post('/user/signin', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-  console.log(req.body);
+  var productList = [];
+  var ordering = " ORDER BY ";
+  switch (req.body.orderby) {
+    case "priceasc":
+      ordering += "price ASC";
+      break;
+    case "pricedes":
+      ordering += "price DESC";
+      break;
+    case "titleasc":
+      ordering += "name ASC";
+      break;
+    case "titledes":
+      ordering += "name DESC";
+      break;
+    default:
+      ordering = "";
+  }
+  var where = " WHERE ";
+  if (req.body.category) {
+    where += "category ";
+  }
+
+  
+  // load all products
+  db.each("SELECT * FROM Products WHERE category = ?" + ordering, req.body.category,
+  function item(err, row) {
+    // add products to list
+    var product = new Product(row.id, row.name, row.price, row.category, row.manufacturer);
+    productList.push(product);
+  },
+  // when query is complete
+  function complete(err, found) {
+    // send information to the view
+    res.render('index', { title: 'Shopping cart', products: productList });
+    //console.log(productList);
+  });
 });
 
 module.exports = router;
